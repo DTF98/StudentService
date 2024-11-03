deaa095705a074cd3df7ecbe7f6e05a9e9ccb617
Denis Boltunov <147542021+DTF98@users.noreply.github.com>
2024-11-03 13:52:46 +0500

    app fix

---
+++ src/main/java/ru/DTF98/StudentService/dto/AuthLoginRequest.java
@@ -0,0 +1,4 @@
+package ru.DTF98.StudentService.dto;
+
+public record AuthLoginRequest(String username, String password) {
+}
---
+++ src/main/java/ru/DTF98/StudentService/dto/AuthResponse.java
@@ -0,0 +1,4 @@
+package ru.DTF98.StudentService.dto;
+
+public record AuthResponse(String message, String token) {
+}
